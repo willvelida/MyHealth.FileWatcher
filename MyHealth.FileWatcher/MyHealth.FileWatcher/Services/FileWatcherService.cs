efd765436c6a95a4184a42b869647d037ff27284
willvelida <willvelida@hotmail.co.uk>
2021-04-07 21:07:13 +1200

    Running code clean up

--- MyHealth.FileWatcher/MyHealth.FileWatcher/Services/FileWatcherService.cs
+++ MyHealth.FileWatcher/MyHealth.FileWatcher/Services/FileWatcherService.cs
@@ -2,11 +2,8 @@
 using Microsoft.Extensions.Configuration;
 using MyHealth.Common;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
 
 namespace MyHealth.FileWatcher.Services
--- MyHealth.FileWatcher/MyHealth.FileWatcher/Services/IFileWatcherService.cs
+++ MyHealth.FileWatcher/MyHealth.FileWatcher/Services/IFileWatcherService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyHealth.FileWatcher.Services
+﻿namespace MyHealth.FileWatcher.Services
 {
     public interface IFileWatcherService
     {
